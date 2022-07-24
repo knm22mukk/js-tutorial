a50ff488fb6067e94a5114d9727ee2d659d0e738
keita.nakamura <76908302+knm22mukk@users.noreply.github.com>
2022-07-24 18:52:11 +0900

    テンプレート文字列``

--- src/index.js
+++ src/index.js
@@ -42,3 +42,15 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+// テンプレート文字列
+// const name = "mukk";
+// const age = 31;
+
+// // 「私の名前はmukkです。年齢は31歳です。」
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+// // テンプレート文字列を用いた方法
+
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
