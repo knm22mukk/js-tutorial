3a4d39f1d8e0dadde37dc41abaec29651e6d06f9
keita.nakamura <76908302+knm22mukk@users.noreply.github.com>
2022-07-25 22:34:39 +0900

    三項演算子を使う

--- src/index.js
+++ src/index.js
@@ -175,3 +175,21 @@ const nameArr = ["tanaka", "yamada", "saitou", "MUKK"];
 //   }
 // });
 // console.log(newNumArr2);
+
+// 三項演算子
+
+// 条件 ? true : false
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(20, 40));
+// console.log(checkSum(50, 60));
